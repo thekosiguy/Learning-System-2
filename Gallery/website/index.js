4c0d535a13abcceaee9a46a415c4c0f3f44bc82e
siband17 <kosi53@hotmail.co.uk>
2018-12-03 00:36:13 +0000

    Log-in function working through utilization of authentication packages however prompts to the user need to be included (for e.g. when the user submits an incorrect password).

--- Gallery/website/index.js
+++ Gallery/website/index.js
@@ -7,33 +7,44 @@ const app = express()
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
+//Initialize authentication packages required for passport
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy;
+
+app.use(require('serve-static')(__dirname + '/../../public'));
+app.use(require('express-session')({
+	secret: 'keyboard cat',
+	resave: true,
+	saveUninitialized: true
+  }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(function(user, done) {
+	done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+	done(null, user);
+});
+
 app.engine('handlebars', handlebars.engine)
 app.set('view engine', 'handlebars')
 
 const port = 8080
 
 app.get('/menu', (req, res) => {
-	var readline = require('readline');
-	var fs = require('fs');
 	res.render('menu')
 })
 
-app.post('/menu', (req, res) => {
-	const formData = JSON.stringify(req.body, null, 2);
-	const jsonObj = JSON.parse(formData);
-
-	const user_data = {
-		username: jsonObj.username,
-		password: jsonObj.password
-		}
-
-	res.redirect('user_input.js', user_data)
-})
-
-app.post('/user', (req, res) => {
-	console.log(req.body);
+app.get('/user', (req, res) => {
 	res.render('user')
-})
+});
+
+app.post('/menu', passport.authenticate
+	('local', {
+	successRedirect: ('/user'),
+	failureRedirect: '/menu'
+})); 
 
 app.get('/register', (req, res) => {
 	var readline = require('readline');
@@ -47,11 +58,11 @@ app.post('/register', (req, res) => {
 	
 	const sqlite3 = require('sqlite3').verbose(); 
 	
-	let userdb = new sqlite3.Database('./users.db', (err) => {;
+	let userdb = new sqlite3.Database('./users.db', (err) => {
 	if (err){
 		return console.error(err.message); 
 	}
-		console.log('Connected to User database.');
+		console.log('Connected to Users database.');
 	})
 															  
 	let sql = userdb.run('INSERT INTO Users(name, house_no, street, city, postcode, gender, dob, emailadd, username, password) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', 
@@ -92,5 +103,41 @@ app.post('/webpage', (req, res) => {
 app.get('/about', (req, res) => {
 	res.render('about')
 })
+
+//Used for validating login information input by the user
+passport.use(new LocalStrategy(
+	function(username, password, done) {
+		console.log("Username:"+ username);
+		console.log("Password:" + password);
+
+		const sqlite3 = require('sqlite3').verbose(); 
+		const userdb = new sqlite3.Database('./users.db'); 
+
+		userdb.get('SELECT password FROM Users WHERE username = ?', [username],
+			function(err, results, fields){
+				if (err) {done(err)}; 
+				
+				if (results != undefined){
+					//Length of returned object is 0 therefore return to menu page
+					if (results.length === 0){
+						done(null, false)
+					}
+					/*Username exists in the database, so compare password input by the user
+					to password stored in the database that corresponds to the username, if
+					they match log the user in.*/
+					if (results.password === password){
+						//log the user in
+						return done(null, {username}); 
+					}else{
+						//Incorrect password therefore return to menu page
+						return done(null, false)
+					}
+				//Username doesn't exist in the database therefore return to menu page
+				}else{
+					done(null, false)
+				}
+			})
+	}
+));
 
 app.listen(port, () => console.log(`app listening on port ${port}`))
